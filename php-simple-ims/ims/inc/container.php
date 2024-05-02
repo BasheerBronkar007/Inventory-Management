4eb6c3f966a37e03fe77e9bbb145fb6ce2371258
Basheer Bronkar <89350333+BasheerBronkar007@users.noreply.github.com>
2024-05-02 12:43:16 +0530

    Add files via upload

---
+++ php-simple-ims/ims/inc/container.php
@@ -0,0 +1,12 @@
+</head>
+<body class="bg-gradient bg-dark bg-opacity-25">
+<nav role="navigation" class="navbar navbar-dark navbar-static-top bg-dark bg-gradient">
+    <div class="container">
+      <div class="navbar-header">
+        <a href="https://www.aiktc.ac.in" class="navbar-brand">Inventory Management System - AIKTC</a>
+      </div>
+    </div>
+  </nav>
+	
+	<div class="container">
+	
